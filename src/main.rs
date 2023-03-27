cbd0c3d13ee63a2f67774905d34caeae355faa07
rishadbaniya <rbdroid01@gmail.com>
2023-03-27 23:31:25 +0545

    Added basic arti client for getting consensus document

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
+use arti_client::TorClient;
+use arti_client::TorClientConfig;
 #[tokio::main]
 async fn main() {
-    println!("This is the best client that's out there haita guys");
+    // Generate the Microdescriptor consensus document at the default storage provided by storage
+    // config
+    // TODO: Make use of it to explore the Relay
+    let config = TorClientConfig::default();
+
+    let tor_client = TorClient::create_bootstrapped(config).await.unwrap();
 }
